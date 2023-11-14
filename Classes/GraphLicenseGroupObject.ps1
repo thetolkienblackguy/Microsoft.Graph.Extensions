a576833dbb162e3b23868337702509fa5e1fa300
Delaney <28851692+thetolkienblackguy@users.noreply.github.com>
2023-11-14 18:30:03 -0500

    Create GraphLicenseGroupObject.ps1

---
+++ Classes/GraphLicenseGroupObject.ps1
@@ -0,0 +1,16 @@
+class GraphLicenseGroupObject {
+    static [object]Create([object]$g, [string]$skuId, [hashtable]$sku_table) {
+        $obj = [ordered]@{}
+        $obj["DisplayName"] = $g.DisplayName
+        $obj["ObjectId"] = $g.Id
+        $obj["GroupTypes"] = $g.GroupTypes -join ", "
+        $obj["SkuId"] = $skuId -join ", "
+        $obj["SkuPartNumber"] = $sku_table[$skuId].SkuPartNumber
+        $obj["OnPremisesSyncEnabled"] = $g.OnPremisesSyncEnabled
+        $obj["OnPremisesNetBiosName"] = $g.OnPremisesNetBiosName
+        $obj["OnPremisesSID"] = $g.OnPremisesSecurityIdentifier
+        $obj["OnPremisesDomainName"] = $g.OnPremisesDomainName
+        return [pscustomobject]$obj
+
+    }
+}
