955c1535bd87a5133f7efc2a203d3ab1b9d24b28
thetolkienblackguy <28851692+thetolkienblackguy@users.noreply.github.com>
2023-12-19 19:06:35 -0500

    Update Get-GraphApplicationRole.ps1

--- Public/Get-GraphApplicationRole.ps1
+++ Public/Get-GraphApplicationRole.ps1
@@ -10,7 +10,7 @@ Function Get-GraphApplicationRole {
         The application id of the application to get the roles for
 
         .PARAMETER OutputType
-        The output type of the cmdlet. Valid values are PSObject, Hashtable, JSON, HttpResponseMessage
+        The output type of the cmdlet. Valid values are PSObject, Hashtable
 
         .EXAMPLE
         Get-GraphApplicationRole -ApplicationId "00000000-0000-0000-0000-000000000000"
@@ -40,42 +40,44 @@ Function Get-GraphApplicationRole {
     
     #>
     [CmdletBinding()]
-    [OutputType(
-        [System.Management.Automation.PSCustomObject],[Hashtable],[System.String]
-        
-    )]
+    [OutputType([System.Management.Automation.PSCustomObject],[Hashtable])]
     param (
         [Parameter(Mandatory=$true,ValueFromPipeline=$true,ValueFromPipelineByPropertyName=$true)]
         [Alias("Id")]
         [guid]$ApplicationId,
         [Parameter(Mandatory=$false)]
         [ValidateSet(
-            "PSObject","Hashtable","JSON"
+            "PSObject","Hashtable"
         
         )]
-        [string]$OutputType = "Hashtable"
+        [string]$OutputType = "PSObject"
     
     )
     Begin {
     } Process {
+        # Invoke-MgGraphRequest parameters
         $invoke_mg_params = @{}
         $invoke_mg_params["Method"] = "GET"
         $invoke_mg_params["Uri"] = "https://graph.microsoft.com/v1.0/applications/$($applicationId)?`$select=appRoles"
         $invoke_mg_params["OutputType"] = $outputType
-        Try {
-            $r = Invoke-MgGraphRequest @invoke_mg_params
         
+        # Invoke-MgGraphRequest
+        Try {
+            # Loop while there is a next link
+            $output_obj = Do {
+                $r = Invoke-MgGraphRequest @invoke_mg_params
+                $r.appRoles
+                $next_link = $r."@odata.nextLink"
+                $invoke_mg_params["Uri"] = $next_link
+
+            } While ($next_link)
         } Catch {
+            # Write the error to the pipeline
             Write-Error $_.Exception.Message -ErrorAction Stop
             
         }
     } End {
-        If ($outputType -eq "JSON") {
-            $r
-        
-        } Else {
-            $r.appRoles
-        
-        } 
+        # Return the output object
+        $output_obj
     }
 }
