12a74cefce0f3f7cd29bf2f6f88b237b3a245098
Delaney <28851692+thetolkienblackguy@users.noreply.github.com>
2024-05-29 17:06:31 -0400

    Create Get-GraphUserDirectoryRoleAssignments.ps1

---
+++ Public/Get-GraphUserDirectoryRoleAssignments.ps1
@@ -0,0 +1,120 @@
+Function Get-GraphUserDirectoryRoleAssignments {
+    <#
+        .DESCRIPTION
+        Gets the directory role assignments for a user
+
+        .SYNOPSIS
+        Gets the directory role assignments for a user
+
+        .PARAMETER UserId
+        Specifies the UserId
+
+        .EXAMPLE
+        Get-GraphUserDirectoryRoleAssignments -UserId "jdoe@contoso.com"
+
+        .INPUTS
+        System.String
+
+        .OUTPUTS
+        System.Management.Automation.PSCustomObject
+
+        .NOTES
+        Author: Gabriel Delaney
+        Date: 05/29/2024
+        Version: 0.0.1
+        Name: Get-GraphUserDirectoryRoleAssignments
+
+        Version History:
+        0.0.1 - Original Release - Gabriel Delaney - 05/29/2024
+
+    #>
+    [CmdletBinding()]
+    [OutputType([System.Management.Automation.PSCustomObject])]
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$UserId
+    
+    )
+    Begin {
+        # Setting the error action preference
+        $ErrorActionPreference = "Stop"
+        $PSDefaultParameterValues = @{}
+        $PSDefaultParameterValues["Add-Member:MemberType"] = "NoteProperty"
+        $PSDefaultParameterValues["Add-Member:Force"] = $true
+
+        # Setting the function name
+        $function = $MyInvocation.MyCommand.Name
+
+        # Properties to select
+        $role_def_properties = @(
+            "DisplayName","Id","Description"
+
+        )
+    } Process {
+        # Get-MgUser 
+        $mg_user = Get-MgUser -filter "Id eq '$userId'"
+        If (!$mg_user) {
+            # Setting the error details
+            $error_details_params = @{}
+            $error_details_params["Message"] = "Resource '$userId' does not exist or one of its queried reference-property objects are not present"
+            $error_details_params["Identity"] = $userId
+            $error_details_params["Function"] = $function
+            $error_details_params["Category"] = "ObjectNotFound"
+            $error_details_params["CategoryTargetType"] = "Microsoft.Graph.User"
+            $write_error_params = Set-ErrorDetails @error_details_params
+
+            # Setting the error message
+            Write-Error @write_error_params
+            Break
+
+        } Else {
+            $id = $mg_user.Id
+        
+        }
+ 
+        # Invoke-MgGraphRequest parameter
+        $invoke_mggraph_params = @{}
+        $invoke_mggraph_params["Uri"] = "https://graph.microsoft.com/v1.0/roleManagement/directory/roleAssignments?`$count=true&`$filter=principalId eq '$id'&`$expand=roleDefinition"
+        $invoke_mggraph_params["Method"] = "GET"
+        $invoke_mggraph_params["Headers"] = @{}
+        $invoke_mggraph_params["Headers"]["ConsistencyLevel"] = "eventual"
+
+        # Add-Member parameters
+        $add_member_params = @{}
+        $add_member_params["MemberType"] = "NoteProperty"
+        $add_member_params["Name"] = "AssignmentState"
+        $add_member_params["Value"] = "Assigned"
+        $add_member_params["Force"] = $true
+
+        # Creating an array list to store the role assignments
+        $role_assignments = [System.Collections.ArrayList]::new()
+        
+        # Getting the role assignments
+        $roles = Do {
+            $r = (Invoke-MgGraphRequest @invoke_mggraph_params)
+            $r.Value
+            $invoke_mggraph_params["Uri"] = $r."@odata.nextLink"
+        
+        # Looping through the results until there are no more results
+        } Until (!$r."@odata.nextLink")
+
+        Foreach ($role in $roles) {
+            # Getting the role
+            $role_def = $role.roleDefinition | Select-Object $role_def_properties
+            $role_def | Add-Member -Name principalId -Value $role.id
+            
+            # Adding the role assignment to the array list
+            [void]$role_assignments.Add($role_def)
+
+        }
+        # Adding the assignment state to the role assignments
+        $role_assignments | Add-Member @add_member_params
+
+        # Returning the role assignments
+        $role_assignments
+    
+    } End {
+
+
+    }
+}
